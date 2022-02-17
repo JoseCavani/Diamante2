f68fe2feb168c4d1cc10a353c34ff3a6e70a35b5
Jose Cavani <Jose.Cavani@gmail.com>
2022-02-16 23:17:17 -0300

    simplificação

--- Diamante2.ConsoleApp1/Program.cs
+++ Diamante2.ConsoleApp1/Program.cs
@@ -19,27 +19,29 @@ namespace Diamante.ConsoleApp1
                 Console.ResetColor();
                 goto volta;
             }
-            int numeroParaALetra = (int)letra; // numero de 65 - 90 ASCII
+            int numeroDoLetraDe65A90 = (int)letra; // numero de 65 - 90 ASCII
 
-            int numeroParaLinhas = numeroParaALetra - 65; //numero de 0 a 25 para as letras do alfabeto
+            int numeroDaLetraDe0A25 = numeroDoLetraDe65A90 - 65; //numero de 0 a 25 para as letras do alfabeto
 
             #endregion
 
 
+            
+
 
       
             #region     Parte de cima e meio
-            for (int linhas = 0; linhas <= numeroParaLinhas ; linhas++) // rodar as linhas ate que chege no numero maximo de linhas
+            for (int linhas = 0; linhas <= numeroDaLetraDe0A25 ; linhas++) // rodar as linhas ate que chege no numero maximo de linhas
                 {
-                    for (int colunas = -(numeroParaLinhas+1)/2; colunas <= numeroParaLinhas + linhas; colunas++)// fazer colunas
+                    for (int colunas = -(numeroDaLetraDe0A25+1)/2; colunas <= numeroDaLetraDe0A25 + (numeroDaLetraDe0A25/2); colunas++)// fazer colunas
     // essa logica determine onde iniciar as colunas 
     // tem que ser inicializado de acordo com o valor minimo maior senão dara errado depois
     //o numero inicilizado é  (-numeroParaLinhas ((0 a 25) + 1)/2) 
     //eu descobri isso fazendo as contas com o valor z (colunas == ((numeroParaLinhas)/2) - linhas) e descobri essa formula, note que ele fica negativo
                     {
-                    if (colunas > ((numeroParaLinhas) / 2) + linhas)
+                    if (colunas > ((numeroDaLetraDe0A25) / 2) + linhas)
                         break;// logica para evitar escrever mais espaçoes do que necessario
-                    if (colunas == ((numeroParaLinhas) / 2) - linhas || colunas == ((numeroParaLinhas) / 2)   + linhas  )
+                    if (colunas == ((numeroDaLetraDe0A25) / 2) - linhas || colunas == ((numeroDaLetraDe0A25) / 2)   + linhas  )
                     //valores minimos || e maxmio da cada linha
                     {
                         Console.Write($"{(char)(65 + linhas)}");// descobrir a letra e imprimir
@@ -56,18 +58,17 @@ namespace Diamante.ConsoleApp1
 
 
             #region Parte de baixo
-            for (int linhas = 1; linhas <= numeroParaLinhas; linhas++)
+            for (int linhas = 1; linhas <= numeroDaLetraDe0A25; linhas++)
             {
-                for (int colunas = -(numeroParaLinhas + 1)/2; colunas <= numeroParaLinhas+(numeroParaLinhas - linhas); colunas++)
-                // na parte de baixo muda a logica de ate onde as colunas vão
-                //aqui tem se que o maior numero maximo de colunas (descoberto por testes com a letra Z) e: numeroParaLinhas+(numeroParaLinhas - i)
+                for (int colunas = -(numeroDaLetraDe0A25 +1)/2; colunas <= numeroDaLetraDe0A25+(numeroDaLetraDe0A25/2); colunas++)
+                // na parte de baixo a logica e igual
                 {
-                    if (colunas > ((numeroParaLinhas) / 2) + (numeroParaLinhas - linhas))
+                    if (colunas > ((numeroDaLetraDe0A25) / 2) + (numeroDaLetraDe0A25 - linhas))
                         break; // logica para evitar escrever mais espaçoes do que necessario
-                    if (colunas == ((numeroParaLinhas) / 2) - (numeroParaLinhas - linhas) || colunas == ((numeroParaLinhas) / 2) + (numeroParaLinhas - linhas ))
+                    if (colunas == ((numeroDaLetraDe0A25) / 2) - (numeroDaLetraDe0A25 - linhas) || colunas == ((numeroDaLetraDe0A25) / 2) + (numeroDaLetraDe0A25 - linhas ))
                     // logica para descobrir o minimo e maximo de cada linha
                     {
-                        Console.Write($"{(char)(numeroParaALetra - linhas)}"); // descobrir a letra e imprimr
+                        Console.Write($"{(char)(numeroDoLetraDe65A90 - linhas)}"); // descobrir a letra e imprimr
 
                     }
                    else
